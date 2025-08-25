019c57db6ad5fd3a99b4ad89cd89541e549b0ecb
Reshzy <reshzy581@gmail.com>
2025-08-25 13:00:29 +0800

    chore(nav,bac): fix BAC role middleware; update top nav links to real pages (PRs, Reports, Supply, BAC, CEO, Accounting, Analytics)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,7 @@
 
                     @auth
                         @if(auth()->user()->can('create-purchase-request'))
-                            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('purchase-requests.*')" class="text-gray-700 hover:text-cagsu-maroon">
+                            <x-nav-link :href="route('purchase-requests.index')" :active="request()->routeIs('purchase-requests.*')" class="text-gray-700 hover:text-cagsu-maroon">
                                 <svg class="w-4 h-4 mr-2 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                                 </svg>
@@ -40,7 +40,51 @@
                         @endif
 
                         @if(auth()->user()->can('view-reports'))
-                            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('reports.*')" class="text-gray-700 hover:text-cagsu-maroon">
+                            <x-nav-link :href="route('reports.pr')" :active="request()->routeIs('reports.*')" class="text-gray-700 hover:text-cagsu-maroon">
+                        @if(auth()->user()->hasRole('Supply Officer'))
+                            <x-nav-link :href="route('supply.purchase-requests.index')" :active="request()->routeIs('supply.purchase-requests.*')" class="text-gray-700 hover:text-cagsu-maroon">
+                                <svg class="w-4 h-4 mr-2 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h18M9 7h9m-9 4h9M3 7h3m0 4H3m3 4H3m6 4h12" />
+                                </svg>
+                                {{ __('Supply') }}
+                            </x-nav-link>
+                        @endif
+
+                        @if(auth()->user()->hasAnyRole(['BAC Chair','BAC Members','BAC Secretariat']))
+                            <x-nav-link :href="route('bac.quotations.index')" :active="request()->routeIs('bac.*')" class="text-gray-700 hover:text-cagsu-maroon">
+                                <svg class="w-4 h-4 mr-2 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h10m-9 4h8" />
+                                </svg>
+                                {{ __('BAC') }}
+                            </x-nav-link>
+                        @endif
+
+                        @if(auth()->user()->hasRole('Executive Officer'))
+                            <x-nav-link :href="route('ceo.purchase-requests.index')" :active="request()->routeIs('ceo.*')" class="text-gray-700 hover:text-cagsu-maroon">
+                                <svg class="w-4 h-4 mr-2 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+                                </svg>
+                                {{ __('CEO') }}
+                            </x-nav-link>
+                        @endif
+
+                        @if(auth()->user()->hasRole('Accounting Office'))
+                            <x-nav-link :href="route('accounting.vouchers.index')" :active="request()->routeIs('accounting.*')" class="text-gray-700 hover:text-cagsu-maroon">
+                                <svg class="w-4 h-4 mr-2 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1" />
+                                </svg>
+                                {{ __('Accounting') }}
+                            </x-nav-link>
+                        @endif
+
+                        @if(auth()->user()->can('view-reports'))
+                            <x-nav-link :href="route('reports.analytics')" :active="request()->routeIs('reports.analytics')" class="text-gray-700 hover:text-cagsu-maroon">
+                                <svg class="w-4 h-4 mr-2 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3v18m-4-8v8M3 13v8m16-14v14m-4-10v10" />
+                                </svg>
+                                {{ __('Analytics') }}
+                            </x-nav-link>
+                        @endif
                                 <svg class="w-4 h-4 mr-2 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
                                 </svg>
